e1d921c475261c830ec221579bc12bd291b3591a
unknown <yejz0715@gmail.com>
2022-06-15 16:34:19 +0900

    create button_component

--- src/Component/Button/index.jsx
+++ src/Component/Button/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./style.css";
-function Button() {
+function Button(props) {
   return (
     <div>
-      <button>주문하기</button>
+      <button>{props.text}</button>
     </div>
   );
 }
