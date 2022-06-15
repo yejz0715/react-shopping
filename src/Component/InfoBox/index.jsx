ce5c5d63fa9ad8345aa0f9540fcadd2da41cc55e
unknown <yejz0715@gmail.com>
2022-06-15 16:37:02 +0900

    add info_icon

--- src/Component/InfoBox/index.jsx
+++ src/Component/InfoBox/index.jsx
@@ -4,7 +4,13 @@ function InfoBox(props) {
   return (
     <div>
       <div id="info_box">
-        <label>이미지</label>
+        <label>
+          <img
+            src="assets/img/infoBox_icon.svg"
+            alt="icon"
+            style={{ width: "24px", height: "24px", margin: "17px" }}
+          />
+        </label>
         <span id="info_text">{props.text}</span>
       </div>
     </div>
