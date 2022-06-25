2fe4dbab77789af6d0a94595dab17bc314cd16bd
unknown <yejz0715@gmail.com>
2022-06-26 03:24:33 +0900

    checkBox 구조 변경

--- src/Component/CheckBox/index.jsx
+++ src/Component/CheckBox/index.jsx
@@ -1,11 +1,13 @@
-import React from "react";
 import "./style.css";
 function CheckBox(props) {
   return (
     <div>
-      <input type="checkbox" id="name" />
-      <label htmlFor="name">{props.children}</label>
-      {/* 태그사이에 있는 내용을 보여줄때 props.children 사용 */}
+      <div>
+        <label>{}</label>
+        <img />
+        <label>{}</label>
+        {/* 태그사이에 있는 내용을 보여줄때 props.children 사용 */}
+      </div>
     </div>
   );
 }
