ee57bcab7ee6f7f05bc6c86e5dea0148155e734f
unknown <yejz0715@gmail.com>
2022-06-27 00:56:21 +0900

    FEAT:conponent_selectBox 생성

--- src/Component/SelectBox/index.jsx
+++ src/Component/SelectBox/index.jsx
@@ -1,13 +1,25 @@
-import React from "react";
-function SelectBox(props) {
+import React, { useState } from "react";
+function SelectBox() {
+  const optionList = [
+    { id: 1, text: "도착하면 연락주세요" },
+    { id: 2, text: "부재시 전화주세요" },
+    { id: 3, text: "집 앞에 놓고 벨 눌러주세요" },
+    { id: 4, text: "직접 수령할게요" },
+  ];
+  // const [list, setList] = useState(false);
+  const [option, setOptions] = useState(optionList[0].text);
+  const handleOnClickOption = (e) => {
+    setOptions(e.target.innerText); //선택한 옵션
+  };
   return (
-    <div>
-      <label>{props.text}</label>
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
+    <div onClick={handleOnClickOption}>
+      <label>{option}</label>
+      <ul id="selectOptions">
+        {optionList.map((option) => (
+          <li key={option.id} onClick={handleOnClickOption}>
+            {option.text}
+          </li>
+        ))}
       </ul>
     </div>
   );
