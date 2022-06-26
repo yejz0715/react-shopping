b22ee04865854db3f0c6688394519b5e18ac59cd
unknown <yejz0715@gmail.com>
2022-06-27 00:57:47 +0900

    UPDATE:component_checkBox 수정

--- src/Component/CheckBox/index.jsx
+++ src/Component/CheckBox/index.jsx
@@ -3,9 +3,9 @@ function CheckBox(props) {
   return (
     <div>
       <div>
-        <label>{}</label>
+        <label>{props.name}</label>
         <img />
-        <label>{}</label>
+        <label>{props.price}</label>
         {/* 태그사이에 있는 내용을 보여줄때 props.children 사용 */}
       </div>
     </div>
