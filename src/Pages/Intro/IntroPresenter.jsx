7566395ef0bf175202bcaaffc5e6f154e4e70757
unknown <yejz0715@gmail.com>
2022-06-27 19:00:56 +0900

    FEAT:intro 컴포넌트 생성+ 버튼 링크 연결

--- src/Pages/Intro/IntroPresenter.jsx
+++ src/Pages/Intro/IntroPresenter.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Button from "../../Component/Button";
 
-const IntroPresenter = () => <div></div>;
+const IntroPresenter = () => (
+  <div>
+    <p id="titleText">싱싱한 과일가게</p>
+    <Link to="/list">
+      <Button text="쇼핑하기" />
+    </Link>
+  </div>
+);
 
 export default IntroPresenter;
