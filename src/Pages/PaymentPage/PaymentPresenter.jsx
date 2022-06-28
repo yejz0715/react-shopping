8902ea73cab2adf908619afddc0ff626468a73a6
unknown <yejz0715@gmail.com>
2022-06-28 23:50:56 +0900

    UPDATE:payment-title, infobox 컴포넌트 추가, css파일->scss파일 변경

--- src/Pages/PaymentPage/PaymentPresenter.jsx
+++ src/Pages/PaymentPage/PaymentPresenter.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import "./style.scss";
+import InfoBox from "../../Component/InfoBox";
+import Title from "../../Component/Title";
 
-const PaymentPresenter = () => <div></div>;
+const PaymentPresenter = (props) => (
+  <div id="payment_container">
+    <div id="bar"></div>
+    <Title text="결제 내역" />
+    <InfoBox textList={props.textList} />
+  </div>
+);
 
 export default PaymentPresenter;
