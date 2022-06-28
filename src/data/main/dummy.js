65d55eb11935ae028b9093b83da722880b3c7b6d
unknown <yejz0715@gmail.com>
2022-06-28 16:41:52 +0900

    ADD:dummy.js 파일 추가

---
+++ src/data/main/dummy.js
@@ -0,0 +1,74 @@
+export const friutList = [
+  {
+    id: 1,
+    name: "딸기",
+    price: 7000,
+    img: "/assets/img_strawberry.png",
+  },
+  {
+    id: 2,
+    name: "멜론",
+    price: 13000,
+    img: "/assets/img_melon.png",
+  },
+  {
+    id: 3,
+    name: "오렌지",
+    price: 9000,
+    img: "/assets/img_orange.png",
+  },
+  {
+    id: 4,
+    name: "수박",
+    price: 8500,
+    img: "/assets/img_watermelon.png",
+  },
+  {
+    id: 5,
+    name: "토마토",
+    price: 6000,
+    img: "/assets/img_tomato.png",
+  },
+  {
+    id: 6,
+    name: "아보카도",
+    price: 12000,
+    img: "/assets/img_avocado.png",
+  },
+  {
+    id: 7,
+    name: "포도",
+    price: 7500,
+    img: "/assets/img_grape.png",
+  },
+  {
+    id: 8,
+    name: "레몬",
+    price: 7000,
+    img: "/assets/img_lemon.png",
+  },
+  {
+    id: 9,
+    name: "체리",
+    price: 9000,
+    img: "/assets/img_cherry.png",
+  },
+  {
+    id: 10,
+    name: "복숭아",
+    price: 10000,
+    img: "/assets/img_peach.png",
+  },
+  {
+    id: 11,
+    name: "파인애플",
+    price: 9500,
+    img: "/assets/img_pineapple.png",
+  },
+  {
+    id: 12,
+    name: "배",
+    price: 8000,
+    img: "/assets/img_pear.png",
+  },
+];
