9f231f8dd3c61d46402b597f578c9ac43a65cea2
unknown <yejz0715@gmail.com>
2022-06-28 16:34:24 +0900

    레이아웃 변경, scss 추가

--- src/Pages/ListPage/ListContainer.js
+++ src/Pages/ListPage/ListContainer.js
@@ -1,34 +1,8 @@
 import React from "react";
+import { friutList } from "../../data/main/dummy";
 import ListPresenter from "./ListPresenter";
-import "./style.css";
 
-import strawberry from "../../img/img_strawberry.png";
-import melon from "../../img/img_melon.png";
-import orange from "../../img/img_orange.png";
-import watermelon from "../../img/img_watermelon.png";
-import tomato from "../../img/img_tomato.png";
-import avocado from "../../img/img_avocado.png";
-import grape from "../../img/img_grape.png";
-import lemon from "../../img/img_lemon.png";
-import cherry from "../../img/img_cherry.png";
-import peach from "../../img/img_peach.png";
-import pineapple from "../../img/img_pineapple.png";
-import pear from "../../img/img_pear.png";
-function ListContainer() {
-  const list = [
-    { id: 1, name: "딸기", price: 7000, img: strawberry },
-    { id: 2, name: "멜론", price: 13000, img: melon },
-    { id: 3, name: "오렌지", price: 9000, img: orange },
-    { id: 4, name: "수박", price: 8500, img: watermelon },
-    { id: 5, name: "토마토", price: 6000, img: tomato },
-    { id: 6, name: "아보카도", price: 12000, img: avocado },
-    { id: 7, name: "포도", price: 7500, img: grape },
-    { id: 8, name: "레몬", price: 7000, img: lemon },
-    { id: 9, name: "체리", price: 9000, img: cherry },
-    { id: 10, name: "복숭아", price: 10000, img: peach },
-    { id: 11, name: "파인애플", price: 9500, img: pineapple },
-    { id: 12, name: "배", price: 8000, img: pear },
-  ];
-  return <ListPresenter list={list} />;
-}
+const ListContainer = () => {
+  return <ListPresenter friutList={friutList} />;
+};
 export default ListContainer;
--- src/Pages/ListPage/ListPresenter.jsx
+++ src/Pages/ListPage/ListPresenter.jsx
@@ -1,25 +1,27 @@
 import React from "react";
-import Button from "../../Component/Button";
-import CheckBox from "../../Component/CheckBox";
-import InfoBox from "../../Component/InfoBox";
+import "./style.scss";
 import Title from "../../Component/Title";
-import SelectBox from "../../Component/SelectBox";
+import InfoBox from "../../Component/InfoBox";
+import Button from "../../Component/Button";
 import { Link } from "react-router-dom";
-const ListPresenter = ({ list }) => (
-  <div class="container">
-    <div id="bar"></div>
-    <Title text="SHOPPING LIST" />
-    <InfoBox text="구매하실 상품을 선택해 주세요!" />
-    <div id="check_box">
-      {list.map((item) => (
-        <CheckBox name={item.name} price={item.price} img={item.img} />
-      ))}
+const ListPresenter = ({ friutList }) => {
+  return (
+    <div id="main_container">
+      <Title text="SHOPPING List" />
+      <InfoBox textList={["구매하실 상품을 선택하주세요"]} />
+      <div id="checkbox_list">
+        {friutList.map((item) => (
+          <div key={item.id}>
+            <label>{item.name}</label>
+            <img src={item.img} alt="friut" width="196px" />
+            <label>{item.price}</label>
+          </div>
+        ))}
+      </div>
+      <Link to="/order">
+        <Button text="구매하기" />
+      </Link>
     </div>
-    <Link to="/order">
-      <Button text="주문하기" />
-    </Link>
-    <SelectBox />
-  </div>
-);
-
+  );
+};
 export default ListPresenter;
