7178d11201bb2160a179ff4f09aeada5c271f8ad
unknown <yejz0715@gmail.com>
2022-06-28 23:43:18 +0900

    UPDATE:orderPage-title,infoBox,button 추가, css파일->scss파일로 변경

--- src/Pages/OrderPage/OrderContainer.js
+++ src/Pages/OrderPage/OrderContainer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import OrderPresenter from "./OrderPresenter";
-import "./style.css";
 
 function OrderContainer() {
   return <OrderPresenter />;
--- src/Pages/OrderPage/OrderPresenter.jsx
+++ src/Pages/OrderPage/OrderPresenter.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import "./style.scss";
+import { Link } from "react-router-dom";
+import Button from "../../Component/Button";
+import Title from "../../Component/Title";
+import InfoBox from "../../Component/InfoBox";
+const OrderPresenter = () => (
+  <div id="order_container">
+    <div id="bar"></div>
+    <Title text="배송지 정보 입력" />
+    <InfoBox textList={["모든 정보는 필수 입력 값입니다."]} />
 
-const OrderPresenter = () => <div></div>;
+    <Link to="/payment">
+      <Button text="결제하기" />
+    </Link>
+  </div>
+);
 
 export default OrderPresenter;
