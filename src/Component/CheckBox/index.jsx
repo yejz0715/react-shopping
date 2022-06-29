0a6fb3dcc89151dd810bef8c474874c2262e3cea
unknown <yejz0715@gmail.com>
2022-06-29 16:18:19 +0900

    UPDATE:checkboxList 요소 타입 string-> object 로 변경

--- src/Component/CheckBox/index.jsx
+++ src/Component/CheckBox/index.jsx
@@ -3,15 +3,17 @@ import classNames from "classnames";
 import { useState } from "react";
 import { useEffect } from "react";
 function CheckBox({ name, price, img, checkList, setCheckList }) {
-  const [isChecked, setCheck] = useState(false);
+  const [isChecked, setCheck] = useState(false); //체크되어있는지 여부,isChecked-boolean
   const checked = () => {
-    setCheck((prev) => !prev);
+    setCheck((prev) => !prev); //클릭하면 상태 반대(true<->false)
   };
+
   useEffect(() => {
     isChecked
-      ? setCheckList([...checkList, name])
-      : setCheckList(checkList.filter((item) => item !== name));
+      ? setCheckList([...checkList, { name: name, price: price }])
+      : setCheckList(checkList.filter((item) => item.name !== name));
   }, [isChecked]);
+
   return (
     <div id="check_container" style={{ backgroundImage: `url(${img})` }}>
       <div
