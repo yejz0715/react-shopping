c99859f0019cfe5177651d253c7c7c90f4d1379f
unknown <yejz0715@gmail.com>
2022-06-30 03:52:32 +0900

    ADD:orderPage-inputText, subtitle 컴포넌트 추가

--- src/Pages/OrderPage/OrderPresenter.jsx
+++ src/Pages/OrderPage/OrderPresenter.jsx
@@ -5,6 +5,8 @@ import Button from "../../Component/Button";
 import Title from "../../Component/Title";
 import InfoBox from "../../Component/InfoBox";
 import SubTitle from "../../Component/SubTitle";
+import InputText from "../../Component/InputText";
+import SelectBox from "../../Component/SelectBox";
 const OrderPresenter = () => (
   <div id="order_container">
     <div id="bar"></div>
@@ -12,9 +14,19 @@ const OrderPresenter = () => (
     <InfoBox textList={["모든 정보는 필수 입력 값입니다."]} />
     <div id="profile_container">
       <div id="profile_box">
-        <SubTitle title="주문자 정보" text="이름과 전화번호를 입력해 주세요" />
+        <SubTitle title="주문자 정보" text="이름과 전화번호를 입력해 주세요." />
+        <InputText title="이름" />
+        <InputText title="전화번호" />
+        <InputText title="주소" />
+        <InputText title="배송 메세지" />
+        {/* <SelectBox /> */}
       </div>
-      <div id="shopping_list"></div>
+      <div id="shopping_list">
+        <SubTitle
+          title="구매 목록"
+          text="장바구니에 담긴 과일들을 확인해주세요!"
+        />
+      </div>
     </div>
     <Link to="/payment">
       <Button text="결제하기" />
