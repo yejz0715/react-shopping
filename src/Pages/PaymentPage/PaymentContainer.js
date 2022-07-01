ea389aabde8a227a0f479797f4733cb2ad6352d3
unknown <yejz0715@gmail.com>
2022-07-01 17:29:09 +0900

    ADD:button-sweetalert2로 팝업창 생성

--- src/Pages/PaymentPage/PaymentContainer.js
+++ src/Pages/PaymentPage/PaymentContainer.js
@@ -1,7 +1,12 @@
 import React from "react";
 import PaymentPresenter from "./PaymentPresenter";
+import Swal from "sweetalert2";
 
 function PaymentContainer() {
+  const testFunc = () => {
+    //주문내역 창
+    Swal.fire("안녕");
+  };
   const textList = [
     "추가 금액 정보 확인",
     "배달 주문",
@@ -9,6 +14,6 @@ function PaymentContainer() {
     "2. 20,000원 미만 구매 고객은 배달료 1,000원 추가",
   ];
 
-  return <PaymentPresenter textList={textList} />;
+  return <PaymentPresenter testFunc={testFunc} textList={textList} />;
 }
 export default PaymentContainer;
--- src/Pages/PaymentPage/PaymentPresenter.jsx
+++ src/Pages/PaymentPage/PaymentPresenter.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.scss";
 import InfoBox from "../../Component/InfoBox";
 import Title from "../../Component/Title";
+import Button from "../../Component/Button";
 
-const PaymentPresenter = (props) => (
+const PaymentPresenter = ({ testFunc, textList }) => (
   <div id="payment_container">
     <div id="bar"></div>
     <Title text="결제 내역" />
-    <InfoBox textList={props.textList} />
+    <InfoBox textList={textList} />
+    <Button text="주문내역 확인" handleOnClick={testFunc} />
   </div>
 );
 
