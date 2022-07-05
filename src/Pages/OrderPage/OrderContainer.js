08d3f0c46746dc8be52facf84d11e13d8d37bafd
unknown <yejz0715@gmail.com>
2022-07-05 15:32:16 +0900

    UPDATE:orderPage 수정

--- src/Pages/OrderPage/OrderContainer.js
+++ src/Pages/OrderPage/OrderContainer.js
@@ -1,6 +1,43 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 import OrderPresenter from "./OrderPresenter";
 
-function OrderContainer() {
-  return <OrderPresenter />;
-}
+const OrderContainer = ({}) => {
+  const location = useLocation();
+  const pathName = location.pathname;
+  useEffect(() => {
+    console.log(pathName);
+  }, [pathName]);
+
+  const [user, setUser] = useState({
+    name: "",
+    phoneNum: "",
+    address: "",
+  });
+  const hanldeOnChangeInput = (e) => {
+    const { name, value } = e.target;
+    const updatedUser = {
+      ...user,
+      [name]: value,
+    };
+    setUser(updatedUser);
+  };
+  const handleUser = () => {
+    console.log(user);
+  };
+
+  const handleOpenPopup = () => {
+    console.log("주문내역 확인");
+  };
+  return (
+    <OrderPresenter
+      user={user}
+      hanldeOnChangeInput={hanldeOnChangeInput}
+      handleUser={handleUser}
+      pathName={pathName}
+      handleOpenPopup={handleOpenPopup}
+    />
+  );
+};
+
 export default OrderContainer;
--- src/Pages/OrderPage/OrderPresenter.jsx
+++ src/Pages/OrderPage/OrderPresenter.jsx
@@ -7,28 +7,54 @@ import InfoBox from "../../Component/InfoBox";
 import SubTitle from "../../Component/SubTitle";
 import InputText from "../../Component/InputText";
 import SelectBox from "../../Component/SelectBox";
-const OrderPresenter = () => (
+const OrderPresenter = ({
+  user,
+  hanldeOnChangeInput,
+  handleUser,
+  pathName,
+  handleOpenPopup,
+}) => (
   <div id="order_container">
     <div id="bar"></div>
     <Title text="배송지 정보 입력" />
     <InfoBox textList={["모든 정보는 필수 입력 값입니다."]} />
     <div id="profile_box">
       <SubTitle title="주문자 정보" text="이름과 전화번호를 입력해 주세요." />
-      <InputText title="이름" />
-      <InputText title="전화번호" />
-      <InputText title="주소" />
-      <SelectBox title="배송 메세지" />
-    </div>
-    <div id="shopping_list">
-      <SubTitle
-        title="구매 목록"
-        text="장바구니에 담긴 과일들을 확인해주세요!"
+      <InputText
+        title="이름"
+        name="name"
+        value={user.name}
+        hanldeOnChangeInput={hanldeOnChangeInput}
       />
+      <InputText
+        title="전화번호"
+        name="phoneNum"
+        value={user.phonNum}
+        hanldeOnChangeInput={hanldeOnChangeInput}
+      />
+      <InputText
+        title="주소"
+        name="address"
+        value={user.address}
+        hanldeOnChangeInput={hanldeOnChangeInput}
+      />
+      {pathName === "/payment" ? <SelectBox title="배송 메세지" /> : null}
     </div>
-    <Link to="/payment">
-      <Button text="결제하기" />
+    {pathName === "/payment" ? (
+      <div id="shopping_list">
+        <SubTitle
+          title="구매 목록"
+          text="장바구니에 담긴 과일들을 확인해주세요!"
+        />
+      </div>
+    ) : null}
+
+    <Link to={pathName === "/order" ? "/payment" : "#"}>
+      <Button
+        text={pathName === "/order" ? "결제하기" : "주문내역확인"}
+        handleOnClick={pathName === "/order" ? handleUser : handleOpenPopup}
+      />
     </Link>
   </div>
 );
-
 export default OrderPresenter;
