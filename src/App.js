54802bdcbdc4b308d7c5220f1157c274d4d7c1c3
unknown <yejz0715@gmail.com>
2022-07-05 18:00:59 +0900

    COMENT:생성할 random 코드의 설명

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AppRouter from "./AppRouter";
+//import IntroContainer from "./Pages/Intro";
 function App() {
-  return <AppRouter />;
+  //  const [code,setCode]=useState('')
+  //1 여기서 만든 상태변수를 ->props로 intro로 넘겨줌
+  //2 intro-container 에서
+  //버튼을 눌렀을때 setCoted에(random코드 생성)
+  //new Date().getTime().toString(36);
+  //3 appRouter 에서 props로 받고
+  //<Router>-<Routes>-안에
+  //조건으로 code의 길이가 0이상이면 다른 것들도 보이게
+  //여기서 코드를 다 넘겨줌<ListContainer code={code}>
+  //else intro만 보이게
+  //const [code, setCode] = useState("");
+
+  return (
+    //  <IntroContainer code={code} setCode={setCode} />
+    <AppRouter />
+  );
 }
 
 export default App;
