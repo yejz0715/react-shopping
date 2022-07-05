e70783b2c3d6040081209b4a7673ad85bfe9794f
unknown <yejz0715@gmail.com>
2022-07-05 11:19:38 +0900

    UPDATE: add orderpage

--- src/Pages/OrderPage/OrderContainer.js
+++ src/Pages/OrderPage/OrderContainer.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import OrderPresenter from "./OrderPresenter";
 
 function OrderContainer() {
-  return <OrderPresenter />;
+  const [isSow, setShow] = useState("");
+  const handleText = (e) => {
+    if (isSow !== 0) setShow(e.target.value);
+    console.log(isSow);
+  };
+  const click = () => {
+    setShow();
+  };
+  return <OrderPresenter handleText={handleText} click={click} />;
 }
 export default OrderContainer;
